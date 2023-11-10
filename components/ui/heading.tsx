257d733edecfb2bdb60bbb9cbb90fc6548150d6d
Kazizo <kazizohajar55@gmail.com>
2023-11-11 01:58:31 +0200

    fixed deployment error

--- components/ui/heading.tsx
+++ components/ui/heading.tsx
@@ -1,13 +1,13 @@
 type HeadingProps = {
-  title: string
+    title: string
 }
 
 const Heading: React.FC<HeadingProps> = ({ title }) => {
-  return (
-    <div>
-      <h1 className="text-xl md:text-3xl font-bold tracking-tight capitalize">{title}</h1>
-    </div>
-  )
+    return (
+        <div>
+            <h1 className="text-xl md:text-3xl font-bold tracking-tight capitalize">{title}</h1>
+        </div>
+    )
 }
 
 export default Heading
